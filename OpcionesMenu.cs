6149712664e1eccce28632ba4f6fe499dd1207c2
AnderP08 <50085533+AnderP08@users.noreply.github.com>
2019-12-03 18:15:27 -0500

    Add files via upload

---
+++ OpcionesMenu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuProyecto
+{
+    public class OpcionesMenu
+    {
+        public void LisOpciones()
+        {
+            Console.Clear();
+
+            int opcion;
+            do{
+                Console.WriteLine("MENU DE OPCIONES" +
+                    "\n\n [1] Perfil" +
+                    "\n [2] Amigos" +
+                    "\n [3] Buscar Amigos" +
+                    "\n [0] Salir");
+                Console.Write("\nIngrese opcion: "); opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        Console.WriteLine("\nSesión cerrada exitosamente");
+                        break;
+
+                    default:
+                        Console.WriteLine("Caracter incorrecto, intentalo de nuevo");
+                        break;
+                }
+            } while (opcion !=0);
+        }
+    }
+}
---
+++ Perfil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuProyecto
+{
+    class Perfil
+    {
+        public Perfil()
+        {
+            ListCaracteristicas = new List<Caracteristica>();
+            ListAmistads = new List<Amistad>();
+            ListSolicitudes = new List<Solicitud>();
+        }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string  Ciudad { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public int Telefono { get; set; }
+
+        public List<Caracteristica> ListCaracteristicas { get; set; }
+
+        public List<Solicitud> ListSolicitudes { get; set; }
+        public List<Amistad> ListAmistads { get; set; }
+    }
+}
